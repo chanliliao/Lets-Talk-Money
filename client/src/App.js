4efa32200160c35dec6110d6bd07d45fd570e7a0
Chan Li Liao <cliao119@gmail.com>
2021-02-27 18:06:11 -0500

    set up detail cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import Details from './components/details/Details';
+
+import useStyles from './styles';
 
 const App = () => {
-  return <div>app</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spaceing={0}
+        alignItems='center'
+        justify='center'
+        style={{ height: '100vh' }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title='Income' />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title='Expanse' />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
