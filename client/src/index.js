146de561fc47937812b0276fb92290b1fa3041a4
Chan Li Liao <cliao119@gmail.com>
2021-03-06 18:10:05 -0500

    connected to speechly

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from './context/context';
+import { SpeechProvider } from '@speechly/react-client';
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId='a28d2011-b18b-4776-b264-2986713c17bc' language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 );
