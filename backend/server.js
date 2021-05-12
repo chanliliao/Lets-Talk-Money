fe80eb029a477e5dae2795e42150570f9c4717c1
Chan Li Liao <cliao119@gmail.com>
2021-05-12 16:43:46 -0400

    set up for heroku;

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ dotenv.config();
 const app = express();
 
 // production set up
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 
