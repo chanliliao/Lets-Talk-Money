64b67efad56cd595f224f6fd7605618615387f35
Chan Li Liao <cliao119@gmail.com>
2021-05-12 19:23:43 -0400

    mongoose model done

---
+++ backend/data/transactions.js
@@ -0,0 +1,30 @@
+const transactions = [
+  {
+    amount: 500,
+    category: 'Salary',
+    type: 'Income',
+    date: '2020-11-16',
+    id: '44c68123-5b86-4cc8-b915-bb9e16cebe6a',
+  },
+  {
+    amount: 225,
+    category: 'Investments',
+    type: 'Income',
+    date: '2020-11-16',
+    id: '33b295b8-a8cb-49f0-8f0d-bb268686de1a',
+  },
+  {
+    amount: 123,
+    category: 'Car',
+    type: 'Expense',
+    date: '2020-11-16',
+    id: '0f72e66e-e144-4a72-bbc1-c3c92018635e',
+  },
+  {
+    amount: 50,
+    category: 'Pets',
+    type: 'Expense',
+    date: '2020-11-13',
+    id: 'c5647dde-d857-463d-8b4e-1c866cc5f83e',
+  },
+];
---
+++ backend/models/transactionModels.js
@@ -0,0 +1,29 @@
+// import for model
+import mongoose from 'mongoose';
+
+const transactionSchema = mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('Transaction', transactionSchema);
+
+export default User;
